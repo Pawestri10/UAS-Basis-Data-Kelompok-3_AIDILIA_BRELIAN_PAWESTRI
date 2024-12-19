08111cd33e50b80f6e59f93ace247d1d006b1050
Pawestri10 <pawestri011005@gmail.com>
2024-12-19 10:05:33 +0700

    Add files via upload

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=192.168.1.3:3306;dbname=db_toko',
+    'username' => 'pawestriwahyuning', 
+    'password' => '12345',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
