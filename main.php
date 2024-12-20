3c260194c87b08d827e0596062f4b4190562dcfa
Pawestri10 <pawestri011005@gmail.com>
2024-12-20 13:55:21 +0700

    Add files via upload

--- main.php
+++ main.php
@@ -42,6 +42,10 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
             ['label' => 'Home', 'url' => ['/site/index']],
             ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
+            ['label' => 'Produk', 'url' => ['/produk/index']],
+            ['label' => 'Pemasok', 'url' => ['/pemasok/index']],
+            ['label' => 'Stok', 'url' => ['/stok/index']],
+            ['label' => 'Transaksi', 'url' => ['/transaksi/index']],
             Yii::$app->user->isGuest
                 ? ['label' => 'Login', 'url' => ['/site/login']]
                 : '<li class="nav-item">'
